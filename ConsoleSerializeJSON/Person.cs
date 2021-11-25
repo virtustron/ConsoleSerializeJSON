44d5c18553fe52a24029c61563f17f39ce8c3507
Vitalii Tron <virtustron@gmail.com>
2021-11-25 13:15:32 +0200

    Add project files.

---
+++ ConsoleSerializeJSON/Person.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleSerializeJSON
+{
+    class Person
+    {
+        public string Name { get; set; }
+
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        
+        
+        public Person()
+        {
+            this.Name = "";
+            this.LastName = "";
+            this.Age = 0;
+        }
+        
+
+        public Person(string Name, string LastName, int Age)
+        {
+            this.Name = Name;
+            this.LastName = LastName;
+            this.Age = Age;
+        }
+
+
+
+
+    }
+}
---
+++ ConsoleSerializeJSON/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Text.Json;
+using System.IO;
+
+namespace ConsoleSerializeJSON
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            // 1. Person
+            Person tom = new Person();
+
+            tom.Name = "Tom";
+            tom.LastName = "Smith";
+            tom.Age = 35;
+
+            // 2. Serialize
+            Console.WriteLine("\nSerialize\n");
+            string json = JsonSerializer.Serialize<Person>(tom);
+                        
+            Console.WriteLine(json);
+
+
+
+            string writePath = @"D:\work\Person.json";
+
+            try
+            {
+                using (StreamWriter sw = new StreamWriter(writePath, false, System.Text.Encoding.Default))
+                {
+                    sw.WriteLine(json);
+                }
+
+                Console.WriteLine("Запись выполнена");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            // ==============================================================================
+
+            string json_from_file = "";
+
+            try
+            {
+                using (StreamReader sr = new StreamReader(writePath))
+                {
+                    json_from_file = sr.ReadToEnd();
+                }                
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+
+            // 3. Deserialize
+            Person restoredPerson = JsonSerializer.Deserialize<Person>(json_from_file);
+
+            Console.WriteLine("\nDeserialize\n");
+            Console.WriteLine(restoredPerson.Name);
+            Console.WriteLine(restoredPerson.LastName);
+            Console.WriteLine(restoredPerson.Age);
+            
+        }
+    }
+}
